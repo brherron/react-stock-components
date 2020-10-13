e0519b6426f5b961af4a42849b6ac0987921b29f
Beau Herron <beauh@tkc32m.com>
2020-10-13 17:36:46 -0500

    Fixed gaps in the info section grid.

--- src/components/InfoSection/InfoElements.js
+++ src/components/InfoSection/InfoElements.js
@@ -25,6 +25,7 @@ export const InfoRow = styled.div`
  display: grid;
  grid-auto-columns: minmax()(auto, 1fr);
  align-items: center;
+ grid-gap: 5rem;
  grid-template-areas: ${({imgStart}) => (imgStart ? `'col2 col1'` : `'col1 col2'`)};
 
  @media screen and (max-width: 768px) {
